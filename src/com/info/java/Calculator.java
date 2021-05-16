37a4289169f49e3621ad45fbb4291c9a5f823455
Hamza Vasser <hamzavasser1989@gmail.com>
2021-05-16 14:44:08 -0400

    HV: Calculator file

---
+++ src/com/info/java/Calculator.java
@@ -0,0 +1,42 @@
+package com.info.java;
+
+public class Calculator {
+	public static void main(String[] args) {
+		int result;
+		result = add(4, 8);
+		System.out.println(result);
+		
+		int tract;
+		tract = sub(4, 3);
+		System.out.println(tract);
+		
+		int divs;
+		divs = divi(24, 2);
+		System.out.println(divs);
+		
+		int multipli;
+		multipli = mult(2, 8);
+		System.out.println(multipli);
+		
+	}
+	
+	public static int add(int math1, int math2) {
+		return math1 + math2;
+	}
+	
+	public static int sub(int take1, int take2) {
+		return take1 - take2;
+	}
+	
+	public static int divi(int div1, int div2) {
+		return div1 / div2;
+	}
+	
+	public static int mult(int mul1, int mul2) {
+		return mul1 * mul2;
+	}
+
+	
+}
+
+
